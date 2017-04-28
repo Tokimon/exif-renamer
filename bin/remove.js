a22627afce60338e93dab58a6322f73a19b957b6
Toke Voltelen <tokevoltelen@gmail.com>
2017-04-28 13:02:09 +0200

    Improve "Remove Doublets" alogrithm and feedback

--- bin/remove.js
+++ bin/remove.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const co = require('co');
+const chalk = require('chalk');
+
 const removeDuplets = require('../lib/removeDublets');
-co(removeDuplets(process.cwd()))
-  .then((files) => console.log(`${files.length} Duplets moved to "__duplets" folder`));
+co(removeDuplets(process.cwd()));
