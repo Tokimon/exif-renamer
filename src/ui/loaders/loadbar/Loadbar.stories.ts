b1a05a90e7cd3338c62622e808534454a1d6fcb1
Toke Voltelen <tokevoltelen@gmail.com>
2020-11-27 16:45:11 +0100

    LoadBar Story

---
+++ src/ui/loaders/loadbar/Loadbar.stories.ts
@@ -0,0 +1,34 @@
+import type { StoryConfig } from '~/definitions/stories.d';
+
+import Loadbar from './Loadbar.template.svelte';
+
+
+
+interface ExampleProps {
+  value: number;
+}
+
+
+
+export default {
+  title: 'Loaders/Loadbar',
+  argTypes: {
+    value: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+        step: 1
+      }
+    }
+  }
+};
+
+export const Default = ({ value }: ExampleProps): StoryConfig => ({
+  Component: Loadbar,
+  props: { value }
+});
+
+Default.args = {
+  value: 50
+};
---
+++ src/ui/loaders/loadbar/Loadbar.template.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Loadbar from './Loadbar.svelte';
+
+  export let value = 0;
+</script>
+
+<Loadbar {value} total={100} />
