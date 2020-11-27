7e09d0484efcda7e2c37dd15bcb4b836b8383505
Toke Voltelen <tokevoltelen@gmail.com>
2020-11-27 16:49:39 +0100

    remove redundant stories

--- src/ui/pages/pages.stories.ts
+++
@@ -1,7 +0,0 @@
-import FileListingPageEx from './file-listing-page/FileListingPage.example.svelte';
-import LoadingPageEx from './loading-page/LoadingPage.example.svelte';
-
-export default { title: 'Pages' };
-
-export const FileListingPage = () => ({ Component: FileListingPageEx });
-export const LoadingPage = () => ({ Component: LoadingPageEx });
