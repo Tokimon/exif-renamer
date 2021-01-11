2346fc9522649e8b662f96408d9165ba6477c548
Toke Voltelen <tokevoltelen@gmail.com>
2021-01-11 01:43:06 +0100

    Make sure each server request can not be executed before the previous has ended

--- src/server/requests/handleRequest.ts
+++ src/server/requests/handleRequest.ts
@@ -7,8 +7,18 @@ interface RequestHandler {
 }
 
 
+const loading = new Set() as Set<string>;
+
 export const handleRequest = () =>
   ipcMain.handle('request', async (_, { type, payload }) => {
-    const { default: cb } = await require(`./handlers/${type}`) as RequestHandler;
-    return cb(payload);
+    if (loading.has(type)) { return; }
+
+    loading.add(type);
+
+    const { default: cb } = require(`./handlers/${type}`) as RequestHandler;
+    const value = await cb(payload);
+
+    loading.delete(type);
+
+    return value;
   });
--- src/ui/1_globals/utils/append-icons.ts
+++ src/ui/1_globals/utils/append-icons.ts
@@ -7,10 +7,6 @@ export default async () => {
 
   const { default: icons } = await import('~/ui/0_assets/svg/inline/icons.svg');
 
-  console.log('--- ICONS ---');
-
-  console.log(typeof icons);
-
   const html = icons.replace('<svg', '<svg style="position:absolute;width:0;height:0"');
   document.body.insertAdjacentHTML('afterbegin', html);
 };
