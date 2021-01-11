f7dc05562136ff4803edf76e47208bec2d1477dd
Toke Voltelen <tokevoltelen@gmail.com>
2021-01-11 01:29:01 +0100

    Add new store structure

---
+++ src/ui/1_globals/stores/exifStore.ts
@@ -0,0 +1,5 @@
+import { writable } from 'svelte/store';
+
+import type { ExifCache } from '~/types/exif.d';
+
+export default writable(new Map() as ExifCache);
---
+++ src/ui/1_globals/stores/pathsStore.ts
@@ -0,0 +1,12 @@
+import { derived } from 'svelte/store';
+import { getActions } from '~/ui/1_globals/utils/serverActions';
+
+import directoryStore from './directoryStore';
+
+export default derived(directoryStore, (directory: string, set: (value: string[]) => void) => {
+  if (!directory) { return set([]); }
+
+  getActions()
+    .findFiles(directory)
+    .then((paths) => set(paths || []));
+});
