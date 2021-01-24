0d57a9cab9b6f11c1bae2637f7aca660517105b2
Toke Voltelen <tokevoltelen@gmail.com>
2021-01-24 01:00:59 +0100

    Set up first test

--- src/server/requests/handlers/find-files.ts
+++ src/server/requests/handlers/find-files.ts
@@ -2,12 +2,16 @@ import uniqueArray from 'vanillajs-helpers/ts/uniqueArray';
 
 import type { FindFilesAction } from '~/types/serverActions.d';
 
-import getSupportedFiles from '~/server/metadata/getSupportedFiles';
+import xmpExtensions from '~/shared/extensions/jsons/xmp-extensions.json';
+import getFilePaths from '~/server/metadata/getFilePaths';
 
 
 
-const findFiles: FindFilesAction = async (directory: string) => {
-  const files = await getSupportedFiles(directory);
+const findFiles: FindFilesAction = async (
+  directory: string,
+  extensions: string[] = xmpExtensions
+) => {
+  const files = await getFilePaths(directory, extensions);
   return uniqueArray(files);
 };
 
