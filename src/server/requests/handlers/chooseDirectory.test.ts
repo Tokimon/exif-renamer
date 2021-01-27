81feea8ee4e622ea5ccee4430e7fb459d3cf748f
Toke Voltelen <tokevoltelen@gmail.com>
2021-01-27 23:13:53 +0100

    Test "chooseDirectory"

---
+++ src/server/requests/handlers/chooseDirectory.test.ts
@@ -0,0 +1,52 @@
+import { dialog } from 'electron';
+import { mocked } from 'ts-jest/utils';
+
+import chooseDirectory from './chooseDirectory';
+
+
+
+jest.mock('electron');
+
+const { showOpenDialog } = mocked(dialog);
+
+
+
+
+interface SetShowOpenDialogReturnValueProps {
+  canceled?: boolean,
+  filePaths?: string[];
+}
+
+
+
+const setShowOpenDialogReturnValue = (
+  { canceled = false, filePaths = [] }: SetShowOpenDialogReturnValueProps
+) =>
+  showOpenDialog.mockReturnValue(
+    Promise.resolve({ canceled, filePaths })
+  );
+
+
+
+describe('server/requests/handlers/chooseDirectory', () => {
+  beforeEach(() => {
+    showOpenDialog.mockReset();
+  });
+
+  test('When dialog is canceled it returns null', async () => {
+    setShowOpenDialogReturnValue({ canceled: true });
+
+    const result = await chooseDirectory();
+
+    expect(result).toBeNull();
+  });
+
+  test('When directory has been chosen it returns the first found directory path', async () => {
+    const filePaths = ['my/first file/path/', 'my/second file/path/'];
+    setShowOpenDialogReturnValue({ filePaths });
+
+    const result = await chooseDirectory();
+
+    expect(result).toBe(filePaths[0]);
+  });
+});
