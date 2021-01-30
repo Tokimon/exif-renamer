4ed0abf3b56df9199d60d9b4e138b56618c56dde
Toke Voltelen <tokevoltelen@gmail.com>
2021-01-31 00:16:59 +0100

    Test `isWebImage`

---
+++ src/ui/1_globals/utils/isWebImage.test.ts
@@ -0,0 +1,21 @@
+import { extensions as web } from '~/shared/extensions/images-web';
+import { extensions as camera } from '~/shared/extensions/images-camera';
+
+import isWebImage from './isWebImage';
+
+
+
+describe('ui/1_globals/utils/fileName', () => {
+  test.each(web)('Returns TRUE for file ending in "%s"', (ext) => {
+    expect(isWebImage('file.' + ext)).toBe(true);
+  });
+
+  test.each(camera)('Returns FALSE for file ending in "%s"', (ext) => {
+    expect(isWebImage('file.' + ext)).toBe(false);
+  });
+
+  // TODO: This is bad (maybe disable for non DEV environments)
+  test('Returns true for paths that includes "http"', () => {
+    expect(isWebImage('http')).toBe(true);
+  });
+});
