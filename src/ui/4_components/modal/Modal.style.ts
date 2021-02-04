9cea64877ad24b1e090805c696eb72233f365122
Toke Voltelen <tokevoltelen@gmail.com>
2021-02-04 01:24:53 +0100

    Add Modal component

---
+++ src/ui/4_components/modal/Modal.style.ts
@@ -0,0 +1,18 @@
+import { css } from '~/ui/1_globals/core/css';
+
+
+
+export const window = css`
+  position: relative;
+  background: white;
+  width: 300px;
+  height: 300px;
+  border-radius: 2rem;
+  box-shadow: 0 0.5rem 1rem rgba(25,25,25,0.3);
+`;
+
+export const closeBtn = css`
+  position: absolute;
+  top: 1.5rem;
+  right: 1.5rem;
+`;
---
+++ src/ui/4_components/modal/Modal.template.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Modal from './Modal.svelte';
+
+  export let width = 50;
+  export let height = 50;
+</script>
+
+<Modal style="width: {width}%; height: {height}%;" on:close {...$$restProps} />
