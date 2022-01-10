7c4ad5e3a52f59da06e58f9f97d34fb26943f74a
Tokimon <tvo@leapwork.com>
2022-01-10 16:19:07 +0100

    Set up button svelte story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,5 +9,12 @@ injectGlobal`
 `;
 
 export const parameters = {
-  layout: 'centered'
+  layout: 'centered',
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      // color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
 };
--- src/ui/2_base/button/Button.svelte
+++ src/ui/2_base/button/Button.svelte
@@ -4,31 +4,35 @@
 
   import { button, buttonColor } from './button.style';
 
+  let _static: boolean = false;
+
+  export let label: string = '';
   export let icon: string = '';
   export let disabled: boolean = false;
+  export { _static as static };
   export let color: string = 'primary';
   export let href: string = '';
   export let target: string = '';
   export let className: string = '';
 
-  const colors = buttonColor(color);
+  const colors = buttonColor(color, disabled || _static);
   const cn = classnames(button, colors, className);
 </script>
 
-{#if disabled}
-  <span aria-disabled="true" class={cn}>
+{#if disabled || _static}
+  <span {...(disabled ? { 'aria-disabled': true } : {})} class={cn}>
     {#if icon}<SvgIcon svg={icon} />{/if}
-    {#if $$slots.default}<span><slot /></span>{/if}
+    {#if label}<span>{label}</span>{/if}
   </span>
 {:else if href}
   <a {href} {target} on:click class={cn}>
     {#if icon}<SvgIcon svg={icon} />{/if}
-    {#if $$slots.default}<span><slot /></span>{/if}
+    {#if label}<span>{label}</span>{/if}
   </a>
 {:else}
   <!-- svelte-ignore component-name-lowercase -->
   <button type="button" on:click class={cn}>
     {#if icon}<SvgIcon svg={icon} />{/if}
-    {#if $$slots.default}<span><slot /></span>{/if}
+    {#if label}<span>{label}</span>{/if}
   </button>
 {/if}
