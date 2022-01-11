49d6ae03633cfc53422393dbb413bad68eca3f90
Tokimon <tvo@leapwork.com>
2022-01-11 15:53:58 +0100

    Set up PathString svelte story

---
+++ src/ui/2_base/path-string/PathString.stories.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Meta,Story,Template } from '@storybook/addon-svelte-csf';
+  import omit from 'lodash/omit';
+  import PathString from './PathString.svelte';
+</script>
+
+
+<Meta
+  title="Base/PathString"
+  argTypes={{
+    width: {
+      control: {
+        type: 'range',
+        min: 5,
+        max: 100,
+        step: 1
+      }
+    },
+    path: { control: 'text' },
+    seperator: { control: 'text' },
+    placeholder: { control: 'text' }
+  }}
+/>
+
+<Template let:args>
+  <PathString {...omit(args, 'width')} style="width:{args.width}%" />
+</Template>
+
+<Story name="Default" args={{ path: 'some/directory/to/find/files/from', width: 70 }} />
--- src/ui/2_base/path-string/PathString.template.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import PathString from './PathString.svelte';
-
-  export let width = 100;
-</script>
-
-<PathString {...$$restProps} style="width:{width}%" />
