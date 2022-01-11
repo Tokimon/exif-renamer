6a439090cb74bcc14f9b1b8b9e14fda4777c7423
Tokimon <tvo@leapwork.com>
2022-01-11 16:34:31 +0100

    Set up DirectoryDisplay svelte story

--- src/ui/3_pieces/directory-display/DirectoryDisplay.template.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import DirectoryDisplay from './DirectoryDisplay.svelte';
-
-  export let width = 100;
-</script>
-
-<DirectoryDisplay {...$$restProps} style="width:{width}%" />
