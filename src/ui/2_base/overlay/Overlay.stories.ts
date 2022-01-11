c4a4b686b3aea83fc20f4714e83f6325fd9ef781
Tokimon <tvo@leapwork.com>
2022-01-11 15:52:51 +0100

    Set up Overlay svelte story

--- src/ui/2_base/overlay/Overlay.stories.ts
+++
@@ -1,9 +0,0 @@
-import type { StoryConfig } from '~/types/stories.d';
-
-import Overlay from './Overlay.template.svelte';
-
-
-
-export default { title: '2_base/Overlay' };
-
-export const Default = (): StoryConfig => ({ Component: Overlay });
--- src/ui/2_base/overlay/Overlay.template.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import Overlay from './Overlay.svelte';
-</script>
-
-<Overlay />
