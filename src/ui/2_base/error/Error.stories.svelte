878cf2d3dc8b6107bf6e4e9b7ddf5eb7f1292941
Tokimon <tvo@leapwork.com>
2022-01-11 14:37:35 +0100

    Set up Error svelte story

---
+++ src/ui/2_base/error/Error.stories.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Meta,Story,Template } from '@storybook/addon-svelte-csf';
+
+  import Error from './Error.svelte';
+</script>
+
+<Meta
+  title="Base/Error"
+  argTypes={{
+    message: { control: "text" },
+  }}
+/>
+
+<Template let:args>
+  <Error>{args.message}</Error>
+</Template>
+
+<Story name="Default" args={{ message: 'Oups some error happened' }} />
--- src/ui/2_base/error/Error.template.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import Error from './Error.svelte';
-
-  export let text: string = '';
-</script>
-
-<Error>{text}</Error>
