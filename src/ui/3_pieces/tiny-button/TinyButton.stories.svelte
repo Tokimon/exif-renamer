c3c1f0cb261459abb45fbc5d2c869beda95a78b6
Tokimon <tvo@leapwork.com>
2022-01-12 16:19:39 +0100

    Set up TinyButton svelte story

---
+++ src/ui/3_pieces/tiny-button/TinyButton.stories.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Meta,Story,Template } from '@storybook/addon-svelte-csf';
+  import omit from 'lodash/omit';
+  import { colors } from '~/ui/1_globals/theme/colors';
+  import TinyButton from './TinyButton.svelte';
+</script>
+
+<Meta
+  title="Pieces/TinyButton"
+  component={TinyButton}
+  argTypes={{
+    label: { control: "text" },
+    icon: { control: "text" },
+    disabled: { control: "boolean", defaultValue: false },
+    static: { control: "boolean", defaultValue: false },
+    color: {
+      control: { type: "select", options: Object.keys(colors) },
+    },
+    href: { control: "text" },
+    onClick: { action: "onClick" },
+  }}
+/>
+
+<Template let:args>
+  <TinyButton
+    {...omit(args, ['onClick'])}
+    target={args.href && '_blank'}
+    on:click={args.onClick}
+  />
+</Template>
+
+<Story name="Default" args={{ label: 'Tiny button' }} />
