56aa0266b87a3142d1ad6e1c555b64d518dad54f
Tokimon <tvo@leapwork.com>
2023-11-13 10:44:12 +0100

    Change svelte preprocess to @sveltejs/vite-plugin-svelte

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+/**
+ * @type {import('vite').UserConfig}
+ */
+export default {
+  plugins: [svelte({ preprocess: vitePreprocess() })],
   resolve: {
     alias: {
       '~': resolve('src'),
@@ -13,4 +15,4 @@ export default defineConfig({
   build: {
     outDir: resolve('frontend'),
   },
-});
+};
